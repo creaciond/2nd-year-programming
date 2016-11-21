625323841a26f0cea4d58d5453a8ef804d1214e8
creaciond <daria.maximova.m@gmail.com>
2016-11-21 15:07:17 +0300

    update code, add templates

--- classwork/2016-11-21 control/control-code.py
+++ classwork/2016-11-21 control/control-code.py
@@ -1,11 +1,22 @@
-
-# 10 баллов. Создать на фласке веб-приложение "Англо-тайский словарь", где
-# можно было бы в текстовом поле ввести английское слово и получить в
-# качестве результата запроса — его перевод на тайский.
 import re
 import os
+import html
 import json
 import codecs
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+def cleanLine(line):
+    rubbishReg = re.compile('<.*?>')
+    additReg = re.compile('\[.*?\]')
+    line = rubbishReg.sub('', line)
+    line = additReg.sub( '', line)
+    line = line.strip(' ')
+    return line
 
 
 # 5 баллов. Скачать отсюда https://yadi.sk/d/e6eos6Czyd4Av архив страниц
@@ -15,22 +26,18 @@ import codecs
 # а значением — английское.
 def makeDict():
     thaiDict = {}
-    thaiReg = re.compile('<tr><td class=th><a (?:.*?)>(.*?)</a></td><td>(?:.*?)</td><td class=pos>(?:.*?)</td><td>(.*?)</td></tr>')
-    rubbishReg = re.compile('<.*?>')
-    additReg = re.compile('\[.*?\]')
+    thaiReg = re.compile('<tr><td class=th><a (?:.*?)>(.*?)</a></td><td>(?:.*?)</td><td class=pos>(?:.+?)</td><td>(.*?)</td></tr>')
     for page in os.listdir('./thai_pages'):
         if page.endswith('.html'):
             with open('./thai_pages/' + page, 'r', encoding='utf-8') as f:
                 for line in f.readlines():
-                    line = re.sub('&#34;', '\"', line)
+                    line = html.unescape(line)
                     res = re.findall(thaiReg, line)
                     if res:
                         for each in res:
-                            thai = rubbishReg.sub('', each[0])
-                            thai = additReg.sub( '', thai)
-                            eng = rubbishReg.sub( '', each[1])
-                            eng = additReg.sub( '', eng)
-                            if eng != '' and not eng.endswith('\"'):
+                            thai = cleanLine(each[0])
+                            eng = cleanLine(each[1])
+                            if thai and eng and eng != '' and not eng.endswith('\"'):
                                 thaiDict[thai] = eng
             print(page)
     return thaiDict
@@ -50,9 +57,32 @@ def dictToJson(pythonDict):
         out.write(engToThai)
 
 
+# 10 баллов. Создать на фласке веб-приложение "Англо-тайский словарь", где
+# можно было бы в текстовом поле ввести английское слово и получить в
+# качестве результата запроса — его перевод на тайский.
+@app.route('/')
+def search():
+    return render_template('index.html')
+
+
+@app.route('/results')
+def results():
+    if request.args:
+        res = []
+        with open('engToThai.json', 'r', encoding='utf-8') as f:
+            dictionary = json.loads(f.read())
+            searchWord = request.args.get('engWord')
+            for item in dictionary:
+                if searchWord == item:
+                    res.append([item, dictionary[item]])
+        return render_template('results.html', query=searchWord, res=res)
+                    
+        
+
 def main():
     dictionary = makeDict()
     dictToJson(dictionary)
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
