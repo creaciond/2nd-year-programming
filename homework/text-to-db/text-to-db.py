80555f21f1c6fa766d5dde288cef17ee616b436f
creaciond <daria.maximova.m@gmail.com>
2016-12-15 14:24:33 +0300

    add system, rename functions
    вот это поворот: wordform — одно слово!

--- homework/text-to-db/text-to-db.py
+++ homework/text-to-db/text-to-db.py
@@ -1,16 +1,34 @@
-def getWordForms():
-    wordForms = []
+import os
+
+
+def getWordforms():
+    wordforms = []
     with open('text.txt', 'r', encoding='utf-8') as f:
         words = f.read().lower().split()
         for word in words:
             word = word.strip(' .,?!\"—()')
             if word:
-                wordForms.append(word)
-    return wordForms
+                wordforms.append(word)
+    return wordforms
+
+
+def useMystem(wordformsPath):
+    mystemPath = '/Users/dariamaximova/Desktop/HSE/Программирование/mystem'
+    # paths for source and goal files
+    sourcePath = '.' + os.sep + wordformsPath
+    goalPath = '.' + os.sep + 'mystemRes.txt'
+    # -cnd == all input to output, each word on new line, context disambiguation
+    mystemCommand = mystemPath + ' ' + sourcePath + ' ' + goalPath + ' ' + '-cnd'
+    os.system(mystemCommand)
+
 
 
 def main():
-    wordForms = getWords()
+    wordforms = getWordforms()
+    with open('wordforms.txt', 'w', encoding='utf-8') as f:
+        line = ' '.join(wordforms)
+        f.write(line)
+    useMystem('wordforms.txt')
 
 
 if __name__ == '__main__':
