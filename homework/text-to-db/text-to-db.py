a15710a6ef1f972004e7d9ca8d713d57a2299769
creaciond <daria.maximova.m@gmail.com>
2016-12-19 11:42:24 +0300

    update lemmaIDs

--- homework/text-to-db/text-to-db.py
+++ homework/text-to-db/text-to-db.py
@@ -1,4 +1,7 @@
 import os
+# from sql import *
+# from sql.aggregate import *
+# from sql.conditionals import *
 
 
 '''tables in database:
@@ -6,62 +9,90 @@ import os
     wordEntries — id, wordform, amark, bmark, isWord, textPosition, lemmaID
 '''
 
-def wordformEntry(word, amark, bmark, isWord, textPosition, dataAr):
-    entry = 'INSERT INTO wordEntries (wordform, amark, bmark, isWord, textPosition, lemmaid) VALUES (\"%s\", %d, %d, %d, %d, 0);' % (word, amark, bmark, isWord, textPosition)
-    dataAr.append(entry)
-    textPosition += 1
-    return dataAr, textPosition
-
-def lemmaEntry(wordform, lemma, data):
-    entry = 'INSERT INTO lemmas (wordform, lemma) VALUES (\"%s\", \"%s\");' % (wordform, lemma)
-    if entry not in data:
-        data.add(entry)
-    return data
-
-
+# retrieve words
 def getWordforms():
     wordforms = []
     insertLineData = []
     with open('text.txt', 'r', encoding='utf-8') as f:
         words = f.read().split()
-        posCount = 0
-        # insert lines for wordEntries table
-        for word in words:
-            # flags for punctuation marks
-            amark = 0
-            bmark = 0
-            wordWithoutMarks = word.strip(' .,?!\"—()')
-            if word != wordWithoutMarks and wordWithoutMarks:
-                if word[0] != wordWithoutMarks[0]:
-                    # first symbol is a puntuation mark; it should be appended before word
-                    amark = 1
-                    if word[0] in '\'\"':
-                        symbol = '\\' + word[0]
-                    else:
-                        symbol = word[0]
-                    insertLineData, posCount = wordformEntry(symbol, 0, 0, 0, posCount, insertLineData)
-                if word[len(word)-1] != wordWithoutMarks[len(wordWithoutMarks)-1]:
-                    # last symbol is a punctuation mark; it will be appended after word
-                    bmark = 1
-            # INSERT for words
-            if wordWithoutMarks:
-                wordforms.append(wordWithoutMarks.lower())
-                insertLineData, posCount = wordformEntry(wordWithoutMarks, amark, bmark, 1, posCount, insertLineData)
-                if bmark == 1:
-                    if word[len(word)-1] in '\'\"':
-                        symbol = '\\' + word[len(word)-1]
-                    else:
-                        symbol = word[len(word)-1]
-                    insertLineData, posCount = wordformEntry(symbol, 0, 0, 0, posCount, insertLineData)
-            # INSERT for punctuation marks
-            else:
-                insertLineData, posCount = wordformEntry(word, 0, 0, 0, posCount, insertLineData)
-    # save insert commands for wordEntries
-    with open('wordEntries.txt', 'w', encoding='utf-8') as f:
-        line = '\r\n'. join(insertLineData)
+    return words
+
+
+# form INSERT lines
+def wordformEntry(id, word, amark, bmark, isWord, textPosition, lemmaID, dataAr):
+    entry = 'INSERT INTO wordEntries (id, wordform, amark, bmark, isWord, textPosition, lemmaid) VALUES (%d, \"%s\", %d, %d, %d, %d, %d);' % (id, word, amark, bmark, isWord, lemmaID, textPosition)
+    dataAr.append(entry)
+    textPosition += 1
+    return dataAr, textPosition
+
+
+def lemmaEntry(count, wordform, lemma, data):
+    entry = 'INSERT INTO lemmas (id, wordform, lemma) VALUES (%d, \"%s\", \"%s\");' % (count, wordform, lemma)
+    if entry not in data:
+        data.add(entry)
+    return data
+
+
+# search lemma ID for a wordform
+def searchLemma(wordform, lemmaIDs):
+    wordform = wordform.lower()
+    if wordform in lemmaIDs:
+        return lemmaIDs[wordform]
+    else:
+        return 0
+
+
+# information for wordforms
+# insert lines for wordEntries table
+def insertWordforms(words, lemmaIDs):
+    insertLineData = []
+    posCount = 0
+    for word in words:
+        # flags for punctuation marks
+        amark = 0
+        bmark = 0
+        wordWithoutMarks = word.strip(' .,?!\"—\(\)\[\]:;')
+        if word != wordWithoutMarks and wordWithoutMarks:
+            if word[0] != wordWithoutMarks[0]:
+                # first symbol is a punсtuation mark; it should be appended before word itself
+                amark = 1
+                if word[0] == '\'':
+                    symbol = 2 * word[0]
+                else:
+                    symbol = word[0]
+                insertLineData, posCount = wordformEntry(posCount, symbol, 0, 0, 0, posCount, searchLemma(wordWithoutMarks, lemmaIDs), insertLineData)
+            if word[len(word) - 1] != wordWithoutMarks[len(wordWithoutMarks) - 1]:
+                # last symbol is a punctuation mark; it will be appended after word itself
+                bmark = 1
+        # INSERT for words
+        if wordWithoutMarks:
+            insertLineData, posCount = wordformEntry(posCount, wordWithoutMarks, amark, bmark, 1, posCount, searchLemma(wordWithoutMarks, lemmaIDs), insertLineData)
+            if bmark == 1:
+                if word[len(word) - 1] == '\'':
+                    symbol = 2 * word[len(word) - 1]
+                else:
+                    symbol = word[len(word) - 1]
+                insertLineData, posCount = wordformEntry(posCount, symbol, 0, 0, 0, posCount, searchLemma(wordWithoutMarks, lemmaIDs), insertLineData)
+        # INSERT for punctuation marks
+        else:
+            insertLineData, posCount = wordformEntry(posCount, word, 0, 0, 0, posCount, searchLemma(wordWithoutMarks, lemmaIDs), insertLineData)
+        # save insert commands for wordEntries
+        with open('wordEntries.txt', 'w', encoding='utf-8') as f:
+            line = '\r\n'.join(insertLineData)
+            f.write(line)
+
+
+def mystemWords(words):
+    lowercaseWords = []
+    for word in words:
+        word = word.lower().strip(' .,?!\"—()')
+        if word:
+            lowercaseWords.append(word)
+    filename = 'lemmasList.txt'
+    with open(filename, 'w', encoding='utf-8') as f:
+        line = '\n'.join(lowercaseWords)
         f.write(line)
-    # wordforms — lowered words for mystem parsing
-    return wordforms
+    return filename
 
 
 def useMystem(wordformsPath):
@@ -79,24 +110,28 @@ def useMystem(wordformsPath):
 def getLemmas(lemmasFile):
     with open(lemmasFile, 'r', encoding='utf-8') as f:
         lemmaEntryData = set()
+        wordLemmaPairs = {}
+        lemmaCount = 0
         for line in f.readlines():
             # if there's a {, there's a pair wordform{lemma} in the line
             if '{' in line:
                 lineItems = line.split('{')
-                lemmaEntryData = lemmaEntry(lineItems[0], lineItems[1].strip('}\n'), lemmaEntryData)
+                lemmaEntryData = lemmaEntry(lemmaCount, lineItems[0], lineItems[1].strip('}\n'), lemmaEntryData)
+                # dictionary — for lemmaIDs further on
+                wordLemmaPairs[lineItems[0]] = lemmaCount
+                lemmaCount += 1
     with open('lemmaEntries.txt', 'w', encoding='utf-8') as f:
-        line = '\r\n'.join(list(sorted(lemmaEntryData)))
+        line = '\r\n'.join(sorted(list(lemmaEntryData)))
         f.write(line)
-
+    return wordLemmaPairs
 
 
 def main():
     wordforms = getWordforms()
-    with open('wordforms.txt', 'w', encoding='utf-8') as f:
-        line = ' '.join(wordforms)
-        f.write(line)
-    lemmasFile = useMystem('wordforms.txt')
-    getLemmas(lemmasFile)
+    # lemmas
+    lemmaIDs = getLemmas(useMystem(mystemWords(wordforms)))
+    insertWordforms(wordforms, lemmaIDs)
+
 
 
 if __name__ == '__main__':
