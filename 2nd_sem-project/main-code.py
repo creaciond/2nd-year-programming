24f8924243fa7c6799b4ffff2fe6bc4434610d98
creaciond <daria.maximova.m@gmail.com>
2017-06-10 21:05:24 +0300

    edit quotes, add more code
    1) Все сообщения и прочие глобальные закавычены в двойные кавычки
    2) Начало кода для вычисления семантической близости

--- 2nd_sem-project/main-code.py
+++ 2nd_sem-project/main-code.py
@@ -2,6 +2,8 @@
 import telebot
 import conf
 import flask
+from pymorphy2 import MorphAnalyzer
+import requests
 
 '''
     BOT
@@ -11,20 +13,116 @@ bot = telebot.TeleBot(conf.TOKEN, threaded=False)
 app = flask.Flask(__name__)
 
 
-# WEBHOOKS
+'''
+    WEBHOOKS
+'''
 # webhook URLs and tokens — in conf.py
-WEBHOOK_URL_BASE = "https://{}:{}".format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/{}/".format(conf.TOKEN)
+WEBHOOK_URL_BASE = 'https://{}:{}'.format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
+WEBHOOK_URL_PATH = '/{}/'.format(conf.TOKEN)
 bot.remove_webhook()
 bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
 
 
+'''
+    WORK WITH INPUT
+'''
+def check_message(message):
+    alphabet = set(list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя '))
+    message = message.strip('.,?!\(\)\[\]\'\"\\').lower()
+    # check for number of words
+    if len(message.split()) <= 2:
+        error_code = 1
+    # check for characters
+    i = 0
+    all_cyrillic = True
+    while i <= len(message) and all_cyrillic:
+        if message[i] not in alphabet:
+            error_code = 2
+            all_cyrillic = False
+    # if everything's good so far, no error
+    if all_cyrillic:
+        error_code = 0
+    return error_code, message.split()
+
+
+def get_closeness(morph_analyzer, words):
+    # semantic_dict = {w1: [distance to w2, distance to w3, etc]}
+    semantic_dict = {word: [] for word in words}
+    link = 'http://rusvectores.org/ru/visual/'
+    parameters = {}
+    for word in words:
+        # change into RusVectores format
+        word = word + '_' + str(morph_analyzer.parse(word).tag.POS)
+        try:
+            response = requests.get(link, params=parameters)
+        except:
+            response = {}
+    return semantic_dict
+
+
+def print_error_input(message):
+    error_code, words = check_message(message)
+    '''
+        0 - ok
+        1 - length of input
+        2 - not in Russian
+    '''
+    if error_code == 1:
+        bot.send_message(message.chat.id, "Кажется, вы ввели два слова или меньше. " +
+                         "Наберите /help, чтобы посмотреть пример ввода, и попробуйте ещё раз!")
+    elif error_code == 2:
+        bot.send_message(message.chat.id, "Кажется, в вашей строке есть символы на латинице. "
+                         + "Бот умеет работать только с кириллицей. "
+                         + "Не расстраивайтесь, наберите /help, чтобы посмотреть пример ввода, и попробуйте ещё раз!")
+    else:
+        bot.send_message(message.chat.id, "Всё хорошо, начинаю работать!")
+    return error_code, words
+
+
+'''
+    BOT MESSAGES
+'''
 # send_welcome() function for /start или /help
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, "Привет! Этот бот умеет вычислять семантическую близость двух слов и строить граф, где отображается близость этих слов. \n" +
-                    "Для того, чтобы получить граф, введите больше одного слова, разделённых пробелом \(например, \"кот кошка играть\"\).")
+    bot.send_message(message.chat.id, "Привет! Этот бот умеет вычислять семантическую близость двух слов и"
+                     + " строить граф, где отображается близость этих слов. \n"
+                     + "Для того, чтобы получить граф, введите команду /get_graph и слова через пробел "
+                     + "\(например, \"кот кошка играть\"\). "
+                     + "Оптимальное количество — от 7 до 20 слов. \n"
+                     + "Чтобы увидеть весь список комманд, наберите /commands.")
 
+
+# /about
+@bot.message_handler(commands='/about')
+def tell_about_yourself(message):
+    bot.send_message(message.chat.id, "Я бот, который может вычислять семантическую близость двух слов! "
+                     + "Меня сделала Даша Максимова, ей можно написать: @zghvebi "
+                     + "или на почту: daria.maximova.m@gmail.com")
+
+
+# /commands
+@bot.message_handler(commands='/commands')
+def tell_commands(message):
+    bot.send_message(message.chat.id, "Вот список комманд: \n"
+                     + "/help — выводит подсказку про бота и пример ввода,\n"
+                     + "/about — про бота и его создателя,\n"
+                     + "/commands — список команд для бота (вы только что использовали это команду),\n"
+                     + "/get_graph — получение графа для введённых слов.")
+
+# messages with words
+@bot.message_handler(commands='/get_graph')
+def do_stuff(message):
+    # input
+    bot.send_message(message.chat.id, "Введите слова, разделённые пробелом \(например, \"кот кошка играть\"\)")
+    # check input
+    bot.send_message(message.chat.id, "Проверяю, всё ли хорошо со вводом!")
+    error_code, words = print_error_input(message)
+    if error_code == 0:
+        # do semantic closeness
+        ma = MorphAnalyzer()
+        sem_closeness = get_closeness(ma, words)
+        # draw graphs
 
 '''
     FLASK
