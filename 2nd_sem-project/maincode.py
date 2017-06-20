c3790f3d61ac0b1018959db7456119c4f6d50d3e
creaciond <daria.maximova.m@gmail.com>
2017-06-20 22:31:32 +0300

    remove second "import os"

--- 2nd_sem-project/maincode.py
+++ 2nd_sem-project/maincode.py
@@ -188,7 +188,6 @@ def webhook():
     MAIN
 '''
 if __name__ == '__main__':
-    import os
     app.debug = True
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
